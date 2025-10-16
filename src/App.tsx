09f6822dca7370f137ea1aa02cd88ef81fd39340
Surya F. Helmianto <fayrushelmianto@apps.ipb.ac.id>
2025-10-16 13:47:52 +0700

    First Deployment
    Bismillah

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">Hello Tailwind 👋</h1>
+      <p className="text-gray-600 mt-2">Tailwind is working!</p>
+    </div>
+  );
+}
+
+export default App;
